59d81091c35d2aa62bd131c6245271b1fd574c68
Joshua Moyers <jmoyers@gmail.com>
2017-08-27 17:39:39 -0700

    Use RStudio instead of visual studio and get me some ggplot

--- Sorts/GraphElapsedTime.R
+++ Sorts/GraphElapsedTime.R
@@ -1,5 +1,9 @@
-library(reshape)
+library(reshape2)
+library(ggplot2)
+
 read.csv("sort_times.csv", header = TRUE) -> sorts
-cast(sorts, Algorithm ~ Input.Size) -> sorts
-matplot(t(sorts), type = "b", pch = 15:19, col = c(1:5), xlab = "Input Size", ylab = "Time Elapsed (s)")
-legend("topleft", inset = 0.01, col = c(1:5), legend = sorts$Algorithm, pch = 15:19, bg = ("white"), horiz = F)
+
+ggplot(sorts, aes(x=Input.Size,
+                  y=Time.Elapsed,
+                  color=Algorithm))+
+  geom_line()
